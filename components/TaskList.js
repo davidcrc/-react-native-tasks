ac48070483a9381de150457f77e108473d59befb
david <david_drcr@hotmail.com>
2021-08-13 12:20:20 -0500

    update data from API

--- components/TaskList.js
+++ components/TaskList.js
@@ -1,21 +1,51 @@
-import React from 'react';
-import {View, Text, FlatList} from 'react-native';
+// rnfe shortcode
+import React, { useState, useEffect } from 'react';
+import { FlatList, RefreshControl } from 'react-native';
 import TaskItem from './TaskItem';
-
+import { getTasks } from '../api';
 const TaskList = props => {
-  const {tasks} = props;
+  const [tasks, setTasks] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
 
-  const renderItem = ({item}) => {
-    // return <Text>{item.title}</Text>;
-    return <TaskItem task={item} />
+  const loadTask = async () => {
+    const data = await getTasks();
+    setTasks(data);
+    // console.log("get tasks", data);
   };
+  useEffect(() => {
+    console.log('loaded');
+    loadTask();
+  }, []);
+
+  const renderItem = ({ item }) => {
+    return <TaskItem task={item} />;
+  };
+
+  // REVIEW: How use refreshing
+  const onRefresh = React.useCallback(async () => {
+    console.log('refrescando');
+    setRefreshing(true);
+    await loadTask();
+    setRefreshing(false);
+    console.log('dejar de refrescando');
+  });
 
   return (
     <FlatList
-      style={{ width: '100%'}}
+      style={{
+        width: '100%',
+      }}
       data={tasks}
-      keyExtractor={item => item.id.toString() }
+      keyExtractor={item => item.id.toString()}
       renderItem={renderItem}
+      refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          colors={['#78e08f']}
+          onRefresh={onRefresh}
+          progressBackgroundColor="#0a3d62"
+        />
+      }
     />
   );
 };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,28 +1,13 @@
 // rnfe shortcode
-import React, { useEffect, useState } from "react";
-import { View, Text,  } from "react-native";
-import { getTasks } from "../api";
+import React from "react";
 import Layout from "../components/Layout";
 import TaskList from "../components/TaskList";
 
-
 const HomeScreen = () => {
-  const [tasks, setTasks] = useState([]);
 
-  const loadTask = async () => {
-    const data = await getTasks();
-    setTasks(data);
-    // console.log("get tasks", data);
-  };
-  useEffect(() => {
-    console.log("loaded");
-    loadTask();
-  }, []);
-
-  // console.log("tasks size", tasks.length);
   return (
     <Layout>
-      <TaskList tasks={tasks} />
+      <TaskList  />
     </Layout>
   );
 };
